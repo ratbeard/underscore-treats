e75f748e2a74f392972e9755180dd7c228fccf72
Mike Frawley <frawl021@gmail.com>
2010-02-28 19:30:38 -0600

    make tests easier to read

--- test/flexargs.js
+++ test/flexargs.js
@@ -1,39 +1,51 @@
 $(document).ready(function() {
+  // msg as last arg is annoying
+  function it (msg, actual, expected) { 
+    same(actual, expected, msg);
+  }
+  
+  var dogs = [
+    {name: 'bubbles', age: 5}, 
+    {name: 'lily', age: 5}, 
+    {name: 'ginger', age: 9}
+  ];
+
 
   module("Flexargs");
 
-  test('collections: filter', function() {
+
+  test('filter', function() {
     var evens = _.select([1, 2, 3, 4, 5, 6], function(num){ return num % 2 == 0; });
     equals(evens.join(', '), '2, 4, 6', 'selected each even number');
 
     evens = _.filter([1, 2, 3, 4, 5, 6], function(num){ return num % 2 == 0; });
     equals(evens.join(', '), '2, 4, 6', 'aliased as "filter"');
 
-    var dogs = [{name: 'bubbles', age: 5}, {name: 'lily', age: 5}, {name: 'ginger', age: 9}]
-    
-    same(
+    it('can filter by passing an object of properties to match',
       _.filter(dogs, {age: 5}),
-      _.filter(dogs, function (dog) { return dog.age == 5; }),
-      'can filter by passing an object of properties to match');
+      _.filter(dogs, function (dog) { return dog.age == 5; })
+    );
 
-    same(
+    it('can filter by passing an object of multiple properties to match',
       _.filter(dogs, {age: 5, name: 'lily'}),
-      _.filter(dogs, function (dog) { return dog.age == 5 && dog.name == 'lily'; }),
-      'can filter by passing an object of multiple properties to match');
+      _.filter(dogs, function (dog) { return dog.age == 5 && dog.name == 'lily'; })
+    );
       
-    same(
+    it('can filter by passing a key and value to match',
       _.filter(dogs, 'age', 5),
-      _.filter(dogs, function (dog) { return dog.age == 5; }),
-      'can filter by passing a key and value to match');
+      _.filter(dogs, function (dog) { return dog.age == 5; })
+    );
       
-    same(
+    it('passing a regex as a value calls regex.test()',
       _.filter(dogs, 'name', /l/),
-      _.filter(dogs, function (dog) { return /l/.test(dog.name) }),
-      'passing a regex as a value calls regex.test()');
+      _.filter(dogs, function (dog) { return /l/.test(dog.name) })
+    );
       
-    same(
+    it('can pass a regex in an object',
       _.filter(dogs, {name: /e/, age: 5}),
-      _.filter(dogs, function (dog) { return /e/.test(dog.name) && dog.age === 5}),
-      'can pass a regex in an object');
+      _.filter(dogs, function (dog) { return /e/.test(dog.name) && dog.age === 5})
+    );
   });
+  
+  
 });
