9dbe944c9391abec500f100eadaaf33f990c3937
Mike Frawley <frawl021@gmail.com>
2010-02-28 20:46:47 -0600

    abstracted out

--- underscore.flexargs.js
+++ underscore.flexargs.js
@@ -1,104 +1,46 @@
 
+flexargs = function (iterator, context) {
+  return (typeof iterator === 'object') ? multicheck(iterator)
+  :      (typeof iterator === 'string') ? singlecheck(iterator, context)
+  :                                       iterator;
+}
 
-checker = function (obj, key, value) {
+singlecheck = function (key, value) {
   return _.isRegExp(value) ? 
     function(item) { return value.test(item[key]);} 
   : function(item) { return item[key] === value; };
 };
 
+multicheck = function (props) {
+  return function (item) { 
+     return _.every(props, function (value, key) {
+       return singlecheck(key, value)(item);
+     });
+   }
+}
 
 
 var origFilter = _.filter;
  _.filter = function(obj, iterator, context) {
-   if (!_.isFunction(iterator)) {
-     if (typeof iterator === 'object') {  // given props to check, e.g. {age: 5, name: 'h' }// TODO: speed up if only one prop
-       var expected = iterator, iterator = function (item) { 
-         return _.every(expected, function (value, key) {
-           return checker(obj, key, value)(item);
-         });
-       };
-     }
-     else
-       iterator = checker(obj, iterator, context);
-   }
-   
-   return origFilter(obj, iterator, context)
+   return origFilter(obj, flexargs(iterator, context), context)
  };
 
 
  var origdetect = _.detect;
-
   _.detect = function(obj, iterator, context) {
-    if (!_.isFunction(iterator)) {
-      
-      if (typeof iterator === 'object') {  
-        var expected = iterator, iterator = function (item) { 
-          return _.every(expected, function (value, key) {
-            return _.isRegExp(value) ? value.test(item[key]) : value === item[key];
-          });
-        };
-      }
-      
-      else {  
-        var key = iterator, value = context;
-        iterator = _.isRegExp(value) ? 
-          function(item) { return value.test(item[key]);} :
-          function(item) { return item[key] === value; };
-      }
-    }
-
-    return origdetect(obj, iterator, context)
-
+    return origdetect(obj, flexargs(iterator, context), context)
   };
 
 
 var origall = _.all
 _.all = function (obj, iterator, context) {
-  iterator = iterator || _.identity;
-  if (!_.isFunction(iterator)) {
-    
-    if (typeof iterator === 'object') {  
-      var expected = iterator, iterator = function (item) { 
-        return _.every(expected, function (value, key) {
-          return _.isRegExp(value) ? value.test(item[key]) : value === item[key];
-        });
-      };
-    }
-    
-    else {  
-      var key = iterator, value = context;
-      iterator = _.isRegExp(value) ? 
-        function(item) { return value.test(item[key]);} :
-        function(item) { return item[key] === value; };
-    }
-  }
-
-  return origall(obj, iterator, context)
+  return origall(obj, flexargs(iterator, context), context)
 }
 
 
 var origany = _.any
 _.any = function (obj, iterator, context) {
-  iterator = iterator || _.identity;
-  if (!_.isFunction(iterator)) {
-    
-    if (typeof iterator === 'object') {  
-      var expected = iterator, iterator = function (item) { 
-        return _.every(expected, function (value, key) {
-          return _.isRegExp(value) ? value.test(item[key]) : value === item[key];
-        });
-      };
-    }
-    
-    else {  
-      var key = iterator, value = context;
-      iterator = _.isRegExp(value) ? 
-        function(item) { return value.test(item[key]);} :
-        function(item) { return item[key] === value; };
-    }
-  }
-
-  return origany(obj, iterator, context)
+  return origany(obj, flexargs(iterator, context), context)
 }
 
 
